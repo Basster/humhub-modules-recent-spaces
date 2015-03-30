021d8c9ad154850bd2ea72ffeb281a23ad2209d6
Wmevi <jexawow@gmail.com>
2015-03-30 14:37:42 +0300

    Updata

---
+++ messages/tr/base.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    '<strong>Recent</strong> spaces' => '<strong>Son</strong> Mekanlar',
+    'No spaces.' => 'Mekan yok.',
+    'Get a list' => 'Liste anzeigen',
+    'Recent Spaces Module Configuration' => 'Neueste Spaces Modul Konfiguration',
+    'You may configure the number of spaces to be shown.' =>
+        'Hier können Sie einstellen, wie viele, der neuesten Spaces, in der Übersicht angezeigt werden sollen',
+    'The number of recent spaces that will be shown.' => 'Wieviele Spaces sollen angezeigt werden?',
+    'Save' => 'Kaydet',
+    'Back to modules' => 'Modüllere geri dön',
+);
