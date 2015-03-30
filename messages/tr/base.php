ed256e1b17a748b39eccb8de4964f324d7020047
Çağatay <JeXawoW@users.noreply.github.com>
2015-03-30 17:52:14 +0300

    Update base.php

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -2,11 +2,10 @@
 return array(
     '<strong>Recent</strong> spaces' => '<strong>Son</strong> Mekanlar',
     'No spaces.' => 'Mekan yok.',
-    'Get a list' => 'Liste anzeigen',
-    'Recent Spaces Module Configuration' => 'Neueste Spaces Modul Konfiguration',
-    'You may configure the number of spaces to be shown.' =>
-        'Hier können Sie einstellen, wie viele, der neuesten Spaces, in der Übersicht angezeigt werden sollen',
-    'The number of recent spaces that will be shown.' => 'Wieviele Spaces sollen angezeigt werden?',
+    'Get a list' => 'Bir liste seçin',
+    'Recent Spaces Module Configuration' => 'Son Mekanlarda Modülü Yapılandır',
+    'You may configure the number of spaces to be shown.' => 'Alanların sayısını göstererek yapılandır',
+    'The number of recent spaces that will be shown.' => 'Son mekanların sayısı?',
     'Save' => 'Kaydet',
     'Back to modules' => 'Modüllere geri dön',
 );
