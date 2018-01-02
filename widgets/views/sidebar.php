57004afd8ecbcb55c5b1dbb3373c7cd1d2c80911
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-01-02 19:01:52 +0900

    Enh: Fix for 1.2+ (#2)
    * Enh: Fix for 1.2+
    
    * Fix: for 1.2+
    
    * Fix: for 1.2+
    
    * Fix: for 1.2+
    
    * Fix: for 1.2+
    
    * Remove unneeded br tag

--- widgets/views/sidebar.php
+++ widgets/views/sidebar.php
@@ -3,47 +3,34 @@
 use humhub\models\Setting;
 use yii\helpers\Html;
 
-humhub\modules\recent_spaces\Assets::register($this);
-$showAsList = !!Setting::Get('showAsList', 'recent_spaces');
+humhub\modules\mostactiveusers\Assets::register($this);
+
+$showMoreButton = !!Setting::Get('showMoreButton', 'recent_spaces');
 ?>
-<div class="panel panel-default<?php if ($showAsList) echo ' spaces-list' ?>"
-     id="recent_spaces-panel">
+<div class="panel panel-default<?php if ($showMoreButton) echo ' spaces-list' ?>"
+     id="panel-recent_spaces">
 
   <!-- Display panel menu widget -->
-  <?php humhub\widgets\PanelMenu::widget(['id' => 'recent_spaces-panel']); ?>
+  <?= \humhub\widgets\PanelMenu::widget(['id' => 'panel-recent_spaces']); ?>
 
   <div class="panel-heading">
-    <?php echo Yii::t(
-      'RecentSpacesModule.base',
-      '<strong>Recent</strong> spaces'
-    ); ?>
+    <?= Yii::t('RecentSpacesModule.base', '<strong>Recent</strong> spaces'); ?>
   </div>
   <div class="panel-body">
     <?php if (count($spaces) === 0): ?>
-      <div class="placeholder"> <?php echo Yii::t(
-          'RecentSpacesModule.base',
-          'No spaces.'
-        ) ?></div>
+      <div class="placeholder"> <?= Yii::t('RecentSpacesModule.base', 'No spaces.') ?></div>
     <?php else: ?>
-      <?php if ($showAsList): ?>
-        <?php echo $this->render('_listSpaces', ['spaces' => $spaces]); ?>
+      <?php if ($showMoreButton): ?>
+        <?= $this->render('_listSpaces', ['spaces' => $spaces]); ?>
       <?php else: ?>
-        <?php echo $this->render('_linkSpaces', ['spaces' => $spaces]); ?>
+        <?= $this->render('_linkSpaces', ['spaces' => $spaces]); ?>
       <?php endif; ?>
     <?php endif; ?>
+
     <div class="footer">
       <hr/>
-      <?php
-      // Button Get a list of most active users
-      echo Html::a(
-        Yii::t('RecentSpacesModule.base', 'Get a list'),
-        ['/recent_spaces/list/list'],
-        [
-          'class'       => 'btn btn-info',
-          'data-target' => '#globalModal',
-        ]
-      );
-      ?>
+
+        <?=  Html::a(Yii::t('RecentSpacesModule.base', 'Get a list'), ['/recent_spaces/list/list'], ['class' => 'btn btn-xl btn-primary', 'data-target' => '#globalModal',]); ?>
     </div>
   </div>
 </div>
