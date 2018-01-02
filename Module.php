90dd30d96147b2591e7ad91d98dd3d6af69ef7eb
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-01-02 11:53:07 +0900

    Fix: for 1.2+

--- Module.php
+++ Module.php
@@ -1,47 +1,34 @@
 <?php
-
 namespace humhub\modules\recent_spaces;
-
-use humhub\models\Setting;
 use Yii;
 use yii\helpers\Url;
-
-class Module extends \humhub\components\Module {
-
-  const NAME = 'recent_spaces';
-
-  /**
-   * On build of the dashboard sidebar widget, add the recent_spaces widget if module is enabled.
-   *
-   * @param type $event
-   */
-  public static function onSidebarInit($event) {
-    if (Yii::$app->hasModule(self::NAME)) {
-
-      $event->sender->addWidget(
-        widgets\Sidebar::className(),
-        [],
-        [
-          'sortOrder' => 199,
-        ]
-      );
+use humhub\models\Setting;
+class Module extends \humhub\components\Module
+{
+    /**
+     * On build of the dashboard sidebar widget, add the recent_spaces widget if module is enabled.
+     *
+     * @param type $event            
+     */
+    public static function onSidebarInit($event)
+    {
+        if (Yii::$app->hasModule('recent_spaces')) {
+            $event->sender->addWidget(widgets\Sidebar::className(), [], ['sortOrder' => 400]);
+        }
     }
-  }
-
-  public function getConfigUrl() {
-    return Url::to(['/recent_spaces/config/config']);
-  }
-
-  /**
-   * Enables this module
-   */
-  public function enable() {
-    if (!Yii::$app->hasModule(self::NAME)) {
-      // set default config values
-      Setting::Set('soSpaces', 5, 'recent_spaces');
+    public function getConfigUrl()
+    {
+        return Url::to(['/recent_spaces/config/config']);
     }
-    parent::enable();
-  }
+    /**
+     * Enables this module
+     */
+    public function enable()
+    {
+        parent::enable();
+        if (Setting::Set('soSpaces', 'recent_spaces') == '') {
+            Setting::Set('soSpaces', 5, 'recent_spaces');
+        }
+    }
 }
-
 ?>
