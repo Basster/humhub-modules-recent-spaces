a5e005fa01d4cd4c02b6dee6393a870a99ac2048
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-01-02 09:28:11 +0900

    Fix: for 1.2+

--- config.php
+++ config.php
@@ -3,14 +3,12 @@
 use humhub\modules\dashboard\widgets\Sidebar;
 
 return [
-  'id'        => 'recent_spaces',
-  'class'     => 'humhub\modules\recent_spaces\Module',
-  'namespace' => 'humhub\modules\recent_spaces',
-  'events'    => [
-    ['class'    => Sidebar::className(),
-     'event'    => Sidebar::EVENT_INIT,
-     'callback' => ['humhub\modules\recent_spaces\Module', 'onSidebarInit'],
+    'id' => 'recent_spaces',
+    'class' => 'humhub\modules\recent_spaces\Module',
+    'namespace' => 'humhub\modules\recent_spaces',
+    'events' => [
+        ['class' => Sidebar::className(), 'event' => Sidebar::EVENT_INIT, 'callback' => ['humhub\modules\recent_spaces\Module', 'onSidebarInit']],
     ],
-  ],
 ];
+
 ?>
