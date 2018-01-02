c04a8ddffba43da4d19f758dcec14297e0488868
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-01-02 09:21:05 +0900

    Enh: Fix for 1.2+

--- models/RecentSpace.php
+++ models/RecentSpace.php
@@ -8,8 +8,6 @@ class RecentSpace extends \humhub\modules\space\models\Space {
 
   public $num_members = 0;
 
-  public $wall_entries = 0;
-
   public static function find() {
 
     /**
@@ -23,7 +21,6 @@ class RecentSpace extends \humhub\modules\space\models\Space {
       [
         '*',
         '(SELECT count(`space_membership`.user_id) FROM `space_membership` WHERE `space_membership`.space_id=`space`.id) as num_members',
-        '(SELECT COUNT(`wall_entry`.id) FROM `wall_entry` WHERE wall_entry.wall_id = `space`.wall_id) as wall_entries',
         //                       '(SELECT count(*) FROM `content` WHERE content.user_id=user.id  AND content.object_model=\'humhub\\\modules\\\post\\\models\\\Post\') as count_posts',
       ]
     );
