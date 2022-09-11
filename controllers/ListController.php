53e4fd6e631453ef6f52f9c6120f793bdae207b0
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2022-09-11 18:39:03 -0400

    Update ListController.php

--- controllers/ListController.php
+++ controllers/ListController.php
@@ -4,49 +4,41 @@ namespace humhub\modules\recent_spaces\controllers;
 
 use humhub\components\behaviors\AccessControl;
 use humhub\components\Controller;
+use yii\data\Pagination;
 use humhub\modules\recent_spaces\models\RecentSpace;
 
 /**
  * @package humhub.modules.recent-spaces.controllers
  * @author  Ole Rößner
  */
-class ListController extends Controller {
+class ListController extends Controller
+{
+    public $pageSize = 10;
 
-  public $pageSize = 10;
+    /**
+     * @inheritdoc
+    */
+    public function behaviors()
+    {
+        return [
+            'acl' => [
+                'class' => AccessControl::class,
+                'guestAllowedActions' => ['list'],
+            ],
+        ];
+    }
 
-  /**
-   * @inheritdoc
-   */
-  public function behaviors() {
-    return [
-      'acl' => [
-        'class'               => AccessControl::class,
-        'guestAllowedActions' => ['list'],
-      ],
-    ];
-  }
+    /**
+     * Shows list of most active users with statistic.
+    */
+    public function actionList() {
+        $query = RecentSpace::find();
+        $countQuery = clone $query;
+        $pagination = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => $this->pageSize]);
+        $query->offset($pagination->offset)->limit($pagination->limit);
 
-  /**
-   * Shows list of most active users with statistic.
-   */
-  public function actionList() {
-    $query = RecentSpace::find();
+        return $this->renderAjax('list', ['spaces' => $query->all(), 'pagination' => $pagination]);
 
-    $countQuery = clone $query;
-    $pagination = new \yii\data\Pagination(
-      ['totalCount' => $countQuery->count(), 'pageSize' => $this->pageSize]
-    );
-    $query->offset($pagination->offset)->limit($pagination->limit);
-
-    return $this->renderAjax(
-      'list',
-      [
-        'spaces'     => $query->all(),
-        'pagination' => $pagination,
-      ]
-    );
-  }
+    }
 
 }
-
-?>
