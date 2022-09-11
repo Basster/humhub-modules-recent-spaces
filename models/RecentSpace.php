3358809685a2914d887b376321b79aaf875a41a8
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2022-09-11 18:40:15 -0400

    Remove dead code

--- models/RecentSpace.php
+++ models/RecentSpace.php
@@ -21,7 +21,6 @@ class RecentSpace extends \humhub\modules\space\models\Space {
       [
         '*',
         '(SELECT count(`space_membership`.user_id) FROM `space_membership` WHERE `space_membership`.space_id=`space`.id) as num_members',
-        //                       '(SELECT count(*) FROM `content` WHERE content.user_id=user.id  AND content.object_model=\'humhub\\\modules\\\post\\\models\\\Post\') as count_posts',
       ]
     );
     $query->where('visibility <> ' . Space::VISIBILITY_NONE);
