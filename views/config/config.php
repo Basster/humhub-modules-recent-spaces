59cee3ef6d4f3433cd94ec8fd16346bc9a379510
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2022-09-11 18:40:54 -0400

    Update config.php

--- views/config/config.php
+++ views/config/config.php
@@ -1,61 +1,44 @@
 <?php
 
-use humhub\compat\CActiveForm;
-use yii\helpers\Html;
+/* @var $this View */
+
+/* @var $model ConfigureForm */
+
+use humhub\libs\Html;
+use humhub\modules\recent_spaces\models\ConfigureForm;
+use humhub\modules\ui\form\widgets\ActiveForm;
+use humhub\modules\ui\view\components\View;
 use yii\helpers\Url;
 
 ?>
 <div class="panel panel-default">
-    <div class="panel-heading"><?php echo Yii::t(
-          'RecentSpacesModule.base',
-          'Most Active Users Module Configuration'
-        ); ?></div>
+    <div class="panel-heading"><?= Yii::t('RecentSpacesModule.base', 'Most Active Users Module Configuration'); ?></div>
     <div class="panel-body">
 
 
-        <p><?php echo Yii::t(
-              'RecentSpacesModule.base',
-              'You may configure the number of spaces to be shown.'
-            ); ?></p>
+        <p><?= Yii::t('RecentSpacesModule.base', 'You may configure the number of spaces to be shown.'); ?></p>
         <br/>
 
-        <?php $form = CActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin(['id' => 'configure-form']) ?>
 
-        <?php echo $form->errorSummary($model); ?>
 
         <div class="form-group">
-            <?php echo $form->labelEx($model, 'noSpaces'); ?>
-            <?php echo $form->textField(
-              $model,
-              'noSpaces',
-              ['class' => 'form-control']
-            ); ?>
-            <?php echo $form->error($model, 'noSpaces'); ?>
+            <?= $form->field($model, 'noSpaces')->textInput(['type' => 'number']); ?>
         </div>
 
         <div class="form-group">
             <div class="checkbox">
                 <label>
-                    <?php echo $form->checkBox(
-                      $model,
-                      'showAsList',
-                      []
-                    ); ?><?php echo $model->getAttributeLabel('showAsList'); ?>
+                    <?= $form->field($model, 'showAsList', [])->checkbox() ?>
                 </label>
             </div>
         </div>
 
         <hr>
-        <?php echo Html::submitButton(
-          Yii::t('RecentSpacesModule.base', 'Save'),
-          ['class' => 'btn btn-primary']
-        ); ?>
-        <a class="btn btn-default"
-           href="<?php echo Url::to(['/admin/module']); ?>"><?php echo Yii::t(
-              'RecentSpacesModule.base',
-              'Back to modules'
-            ); ?></a>
-
-        <?php CActiveForm::end(); ?>
+        <div class="form-group">
+        <?= Html::submitButton(Yii::t('RecentSpacesModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+        <a class="btn btn-default" href="<?= Url::to(['/admin/module']); ?>"><?= Yii::t('RecentSpacesModule.base', 'Back to modules'); ?></a>
+        </div>
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
